526c2f8d9f5960872bd72d1f2c684391942129b0
Déborah Martins <deborahamartins29@gmail.com>
2023-05-17 19:27:07 -0300

    aula-1

---
+++ pages/professores/index.js
@@ -0,0 +1,12 @@
+import Pagina from '@/components/Pagina'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Pagina titulo='Professores'>
+
+    </Pagina>
+  )
+}
+
+export default index
---
+++ pages/salas/index.js
@@ -0,0 +1,12 @@
+import Pagina from '@/components/Pagina'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Pagina titulo='Salas'>
+
+    </Pagina>
+  )
+}
+
+export default index
